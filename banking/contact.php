dd34738852244226774eea145eef904cf4c8ae88
smartsiva <52667587+smartsiva@users.noreply.github.com>
2020-04-13 14:30:29 +0530

    Add files via upload

---
+++ banking/contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Chennai Branch</u></h3>
+            <p><span class="heading">Address - </span>1st Ave, Indira Nagar, Adyar, Chennai, Tamil Nadu 600020</p>
+            <p><span class="heading">Tel - </span>044-4568692/12</p>
+            <p><span class="heading">Email - </span>chennaibranch@sssbank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Thiruvallur Branch</u></h3>
+            <p><span class="heading">Address - </span>Manavala nagar ,Thiruvallur</p>
+            <p><span class="heading">Tel - </span>044-4556856/32</p>
+            <p><span class="heading">Email - </span>thiruvallurbranch@sssbank.com</p>
+            
+             <h3 style="color:#2E4372;"><u>Sriperumbudur Branch</u></h3>
+            <p><span class="heading">Address - </span>70, Gandhi Rd, Ramanujar Nagar, Sriperumbudur, Tamil Nadu 602105</p>
+            <p><span class="heading">Tel - </span>044-4556854/11</p>
+            <p><span class="heading">Email - </span>sriperumbudurbranch@sssbank.com</p>
+            </div>
+            </div>
